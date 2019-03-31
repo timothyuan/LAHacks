8be124a47f9654ed6e0ef08f51243065b1a5b759
crystalrain125 <cxu1169@gmail.com>
2019-03-31 00:46:41 -0700

    Changes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ClassifyComponent } from './classify/classify.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
---
+++ client/src/app/name-editor/name-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-name-editor',
+  templateUrl: './name-editor.component.html',
+  styleUrls: ['./name-editor.component.css']
+})
+export class NameEditorComponent {
+  name = new FormControl('');
+}
