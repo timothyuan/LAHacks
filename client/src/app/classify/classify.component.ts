644ef770f94ed8abe6304b25b2644072568cb899
Timothy Yuan <timothyuan@gmail.com>
2019-03-31 06:26:39 -0700

    Merge branch 'master' of github.com:timothyuan/LAHacks

--- client/src/app/classify/classify.component.ts
+++ client/src/app/classify/classify.component.ts
@@ -8,7 +8,6 @@ import { HttpService } from '../http.service';
   styleUrls: ['./classify.component.css']
 })
 export class ClassifyComponent implements OnInit {
-  symptomForm: FormGroup;
   symptoms = [];
   external_text = "Please upload a picture of external symptoms."
   external_desc = "Describe the uploaded picture";
@@ -17,7 +16,7 @@ export class ClassifyComponent implements OnInit {
   possibleSymptoms = ['Spoon-Shaped Fingernails (Koilonychia)', 'Squamous Cell Cancer (Skin Cancer Symptoms and Signs)', 'Squamous Cell Carcinoma (Skin Cancer Symptoms and Signs)', 'Sternutation (Sneezing)', 'Stiff Neck', 'Stomach Cramps', 'Stomach Ulcer (Peptic Ulcer) Symptoms and Signs', 'Stool Color & Texture Changes', 'Strange Behavior (Unusual Behavior)', 'Strep Throat Symptoms and Signs', 'Stroke Symptoms and Signs', 'Sty Symptoms and Signs', 'Subconjunctival Hemorrhage Symptoms and Signs', 'Suicide', 'Swimmers Ear (Otitis Externa) Symptoms and Signs', 'Swine Flu Symptoms and Signs', 'Swollen Ankles and/or Swollen Feet', 'Swollen Breast', 'Swollen Eyes', 'Swollen Gums (Pericoronitis Symptoms and Signs)', 'Swollen Joints', 'Swollen Knee', 'Swollen Lip', 'Swollen Lymph Nodes', 'Swollen Testicles', 'Swollen Tongue', 'Swollen Tonsils', 'Tachycardia', 'Tension Pneumothorax', 'Testicular Pain', 'Testicular Swelling (Swollen Testicles)', 'Thirst', 'Throat Cancer Symptoms and Signs', 'Thrush (Oral Candidiasis) Symptoms and Signs', 'Tic', 'Tightness in Chest', 'Tingling in Hands and Feet', 'Toe Pain', 'Toothache', 'Traumatic Brain Injury (Concussion Symptoms and Signs)', 'Tremor', 'Tuberculosis (TB)', 'Tunnel Vision', 'Vaginal Bleeding', 'Vaginal Discharge', 'Vaginal Dryness', 'Vaginal Itching', 'Vaginal Odor', 'Vaginal Pain', 'Vaginal Yeast Infection Symptoms and Signs', 'Vertical Ridges on the Fingernails', 'Vertigo', 'Vision Loss', 'Vitamin B12 Deficiency Symptoms and Signs', 'Vocal Outbursts', 'Vocal Tics (Tic)', 'Vomiting', 'Vomiting Blood', 'Watery Eye', 'Weakness', 'Weight Gain', 'Weight Loss', 'West Nile Virus Symptoms and Signs', 'Wet AMD (Macular Degeneration Symptoms and Signs)', 'Wheezing', 'White Tongue', 'Wrist Pain', 'Yawning', 'Zika Virus Infection Symptoms and Signs'];
 
 
-  constructor(private fb: FormBuilder, private httpService: HttpService) {
+  constructor(private httpService: HttpService) {
     // const formControls = this.symptoms.map(control => new FormControl(false));
     // this.symptomForm = this.fb.group({
     //   symptoms: new FormArray(formControls)
@@ -28,10 +27,7 @@ export class ClassifyComponent implements OnInit {
   }
 
   submitInt(){
-    const selectedPreferences = this.symptomForm.value.symptoms
-    .map((checked, index) => checked ? this.symptoms[index].id : null)
-    .filter(value => value !== null);
-    this.httpService.classify(selectedPreferences).subscribe(
+    this.httpService.classify(this.symptoms).subscribe(
       result => {
         console.log(result);
       },
@@ -39,6 +35,7 @@ export class ClassifyComponent implements OnInit {
         console.log('error');
       }
     );
+    this.symptoms = [];
   }
 
   submitImg(){
@@ -58,7 +55,5 @@ export class ClassifyComponent implements OnInit {
       this.symptoms.push(value);
     }
   }
-
-  get formData() { return <FormArray>this.symptomForm.get('symptoms'); }
 
 }
